943830250cf319727dfe3926b12bfbc17ff4cc0c
eshhusv <ak7527203@gmail.com>
2023-03-03 19:26:31 +0200

    Add project files.

---
+++ twodottwo/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.Write("a) ");
+if (c > b && b > a) Console.WriteLine("Условие выполняется");
+else Console.WriteLine("Условие не выполняется");
+Console.Write("б) ");
+if (b > a && a > c) Console.WriteLine("Условие выполняется");
+else Console.WriteLine("Условие не выполняется");
